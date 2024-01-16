3d95c6db861adf1e3debec43aad2c41ff6946190
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-16 22:15:43 +0700

    create crud

--- routes/mangaDirektori.js
+++ routes/mangaDirektori.js
@@ -12,4 +12,30 @@ router.get("/", async function (req, res, next) {
   }
 });
 
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await mangaDirektori.create(req.body));
+  } catch (err) {
+    console.error(`Error membuat data manga`, err.message);
+    next(err);
+  }
+});
+
+router.put("/:id", async function (req, res, next) {
+  try {
+    res.json(await mangaDirektori.update(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error Update data manga`, err.message);
+    next(err);
+  }
+});
+
+router.delete("/:id", async function (req, res, next) {
+  try {
+    res.json(await mangaDirektori.remove(req.params.id));
+  } catch (err) {
+    console.error(`Error saat menghapus manga`, err.message);
+    next(err);
+  }
+});
 module.exports = router;
--- service/mangaDirektori.js
+++ service/mangaDirektori.js
@@ -14,6 +14,65 @@ async function getMultiple(page = 1) {
   };
 }
 
+async function create(mangaData) {
+  try {
+    const result = await db.query(
+      `INSERT INTO manga 
+      (judul, penulis, penerbit, tanggal_rilis, rating, jumlah_volume, url_baca) 
+      VALUES 
+      ('${mangaData.judul}', '${mangaData.penulis}', '${mangaData.penerbit}', '${mangaData.tanggal_rilis}', ${mangaData.rating}, ${mangaData.jumlah_volume}, '${mangaData.url_baca}')`
+    );
+
+    if (result.affectedRows) {
+      return { message: "Manga created successfully" };
+    } else {
+      return { message: "Error in creating manga" };
+    }
+  } catch (error) {
+    // Handle any database errors here
+    return { message: "Error in creating manga" };
+  }
+}
+
+async function update(id, mangaData) {
+  try {
+    const result = await db.query(
+      `UPDATE manga 
+      SET judul="${mangaData.judul}", penulis="${mangaData.penulis}", penerbit="${mangaData.penerbit}", 
+      tanggal_rilis="${mangaData.tanggal_rilis}", rating=${mangaData.rating}, jumlah_volume=${mangaData.jumlah_volume}, 
+      url_baca="${mangaData.url_baca}"
+      WHERE id=${id}`
+    );
+
+    if (result.affectedRows) {
+      return { message: "Manga updated successfully" };
+    } else {
+      return { message: "Error in updating manga" };
+    }
+  } catch (error) {
+    // Handle any database errors here
+    return { message: "Error in updating manga" };
+  }
+}
+
+async function remove(id) {
+  try {
+    const result = await db.query(`DELETE FROM manga WHERE id=${id}`);
+
+    if (result.affectedRows) {
+      return { message: "Manga berhasil dihapus" };
+    } else {
+      return { message: "Error saat menghapus" };
+    }
+  } catch (error) {
+    // Handle any database errors here
+    return { message: "Error saat menghapus" };
+  }
+}
+
 module.exports = {
   getMultiple,
+  create,
+  update,
+  remove,
 };
